69058b773b496b5279f100451ee6d6a13ac91218
anaskanafani <anas.kanafani10@outlook.com>
2024-07-26 15:31:24 +0400

    try different model

--- mlapi.py
+++ mlapi.py
@@ -6,17 +6,42 @@ import pandas as pd
 
 app = FastAPI()
 
-class ScoringItem(BaseModel): 
-    YearsAtCompany: float #/ 1, // Float value 
-    EmployeeSatisfaction: float #0.01, // Float value 
-    Position:str # "Non-Manager", # Manager or Non-Manager
-    Salary: int #4.0 // Ordinal 1,2,3,4,5
+class BankNote(BaseModel):
+    variance: float
+    skewness: float
+    curtosis: float
+    entropy: float
 
-with open('rfmodel.pkl', 'rb') as f: 
-    model = pickle.load(f)
+# class ScoringItem(BaseModel): 
+#     YearsAtCompany: float #/ 1, // Float value 
+#     EmployeeSatisfaction: float #0.01, // Float value 
+#     Position:str # "Non-Manager", # Manager or Non-Manager
+#     Salary: int #4.0 // Ordinal 1,2,3,4,5
 
-@app.post('/')
-async def scoring_endpoint(item:ScoringItem): 
-    df = pd.DataFrame([item.dict().values()], columns=item.dict().keys())
-    yhat = model.predict(df)
-    return {"prediction":int(yhat)}
+# with open('rfmodel.pkl', 'rb') as f: 
+#     model = pickle.load(f)
+
+# @app.post('/')
+# async def scoring_endpoint(item:ScoringItem): 
+#     df = pd.DataFrame([item.dict().values()], columns=item.dict().keys())
+#     yhat = model.predict(df)
+#     return {"prediction":int(yhat)}
+
+@app.post("/predict")
+async def predict(data: BankNote):
+    print(data)
+    data = data.dict()
+    print(data)
+    variance = data['variance']
+    skewness = data['skewness']
+    curtosis = data['curtosis']
+    entropy = data['entropy']
+
+    prediction = classifier.predict([[variance, skewness, curtosis, entropy]])
+    print(prediction)
+    if prediction[0] > 0.5:
+        prediction = "Fake note"
+    else:
+        prediction = "Its a Bank note"
+    return {"prediction": prediction}
+    
