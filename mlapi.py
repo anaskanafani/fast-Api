69323892f0b4ae1514ccaea28a8a9e31c6c20e3a
anaskanafani <anas.kanafani10@outlook.com>
2024-07-26 15:37:44 +0400

    fix model #2

--- mlapi.py
+++ mlapi.py
@@ -18,8 +18,8 @@ class BankNote(BaseModel):
 #     Position:str # "Non-Manager", # Manager or Non-Manager
 #     Salary: int #4.0 // Ordinal 1,2,3,4,5
 
-# with open('rfmodel.pkl', 'rb') as f: 
-#     model = pickle.load(f)
+with open('rfmodel.pkl', 'rb') as f: 
+    classifier = pickle.load(f)
 
 # @app.post('/')
 # async def scoring_endpoint(item:ScoringItem): 
@@ -41,7 +41,7 @@ async def predict(data: BankNote):
     curtosis = data['curtosis']
     entropy = data['entropy']
 
-    prediction = rfmodel.predict([[variance, skewness, curtosis, entropy]])
+    prediction = classifier.predict([[variance, skewness, curtosis, entropy]])
     print(prediction)
     if prediction[0] > 0.5:
         prediction = "Fake note"
